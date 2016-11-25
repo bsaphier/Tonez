6987a1cb3fa32e730484dc6bdc8bbf41dbad22e9
Benjamin Saphier <b.saphier@gmail.com>
2016-11-25 13:23:50 -0500

    default commit message

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const nunjucks = require('nunjucks');
 
 const app = express();
+const port = 1337;
 
-var port = 1337;
+nunjucks.configure('views', { noCache: true });
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res, next) => {
+  res.render('layout');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  const err = new Error('errororororor');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.sendStatus(err.status || 500);
+});
+
 app.listen(port, function () {
-  console.log('The server is listening closely on port', port);
+  console.log('ooooh my ping', port);
 });
